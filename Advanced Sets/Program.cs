6ebeb6eaa7b1767cc874d675991be841a4fd320d
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-19 15:42:30 +0200

    Testing the generic set.

--- Advanced Sets/Program.cs
+++ Advanced Sets/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.Globalization;
 using Advanced_Sets.Set;
+using Advanced_Sets.GenericSets;
 namespace Advanced_Sets
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string dd = "{6,898'0,5689'1,256'{5,62'3,25}}";
+            GenericSet<double> gg = new GenericSet<double>(dd,'\'');
+            Console.WriteLine(gg.ElementString);
+
+
+            dd = "{ 8, 7, 5, 2, 14, 1, { 5, 8, 9, 6, 33 } }";
+            GenericSet<int> ii = new GenericSet<int>(dd, ',');
+            Console.WriteLine(ii.ElementString);
+
+
             string expression = "{5,5,3,{6,{5,8},{5,3},5,6},{6,{5,8},{5,3},5,6},7}";
 
             Console.WriteLine("The expression is : " + expression);
