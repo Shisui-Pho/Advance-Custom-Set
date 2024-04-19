ca0de10b179b2c766e2749c3ee88ecd0064ecf7f
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-19 17:10:00 +0200

    Test data

---
+++ Advanced Sets/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+namespace Advanced_Sets
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }//class
+}//namespace
--- Advanced Sets/Program.cs
+++ Advanced Sets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Sets;
 using Sets.Generic;
 namespace Advanced_Sets
@@ -7,18 +8,27 @@ namespace Advanced_Sets
     {
         static void Main(string[] args)
         {
-            string dd = "{6,898'0,5689'1,256'{5,62'3,25}}";
-            GenericSet<double> gg = new GenericSet<double>(dd,'\'');
-            Console.WriteLine(gg.ElementString);
-
-
-            dd = "{ 8, 7, 5, 2, 14, 1, { 5, 8, 9, 6, 33 } }";
-            GenericSet<int> ii = new GenericSet<int>(dd, ',');
-            Console.WriteLine(ii.ElementString);
-
+            //Test CSet
+            //TestCSet();
+            //TestGenericSetWithNumbers();
+            Person p = new Person() { Name = "Phiwo", Surname = "Kwakhe" };
+            string s = p.ToString();
+            Console.WriteLine(p);
+            //Person a = (Person)Convert.ChangeType(s, typeof(Person));
+            PropertyInfo info = typeof(Person).GetProperty(s);
+            
+            Console.ReadLine();
+        }//Main
+        private static void TestCSet()
+        {
+            Console.WriteLine("TESTING CSet : Non-Generic set");
+            Console.WriteLine("===============================");
+            Console.WriteLine();
+            Console.WriteLine("Please note that the sorting may not be acurate because it does a string sorting!");
+            Console.WriteLine();
+            Console.WriteLine();
 
             string expression = "{5,5,3,{6,{5,8},{5,3},5,6},{6,{5,8},{5,3},5,6},7}";
-
             Console.WriteLine("The expression is : " + expression);
             CSet set = new CSet(expression);
             Console.WriteLine("The element string will be : " + set.ElementString);
@@ -80,8 +90,97 @@ namespace Advanced_Sets
             set = new CSet(expression);
             Console.WriteLine("The element string will be : " + set.ElementString);
             Console.WriteLine("The cardinality will be : " + set.Cardinality);
-
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Finnished!!!");
+            Console.Write("Press any key to see the generic set(with integers : generic version of the above data).......");
             Console.ReadLine();
-        }//Main
+            Console.Clear();
+        }//TestCSet
+        private static void TestGenericSetWithNumbers()
+        {
+            Console.WriteLine("Test Generic set : Using intagers");
+            Console.WriteLine("=================================");
+            Console.WriteLine("Note that the integers are seperated by a comma (\",\")");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string expression = "{5,5,3,{6,{5,8},{5,3},5,6},{6,{5,8},{5,3},5,6},7}";
+            Console.WriteLine("The expression is : " + expression);
+            GenericSet<int> set = new GenericSet<int>(expression, ',');
+            Console.WriteLine("The element string will be : " + set.ElementString);
+            Console.WriteLine("The cardinality will be : " + set.Cardinality);
+
+            expression = "{{{{{{{{6}}}}}}}}";
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The expression is : " + expression);
+            set = new GenericSet<int>(expression, ',');
+            Console.WriteLine("The element string will be : " + set.ElementString);
+            Console.WriteLine("The cardinality will be : " + set.Cardinality);
+
+            expression = "{1,{2,{5},{7}},{8}}";
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The expression is : " + expression);
+            set = new GenericSet<int>(expression, ',');
+            Console.WriteLine("The element string will be : " + set.ElementString);
+            Console.WriteLine("The cardinality will be : " + set.Cardinality);
+
+            expression = "{5,2,1,0}";
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The expression is : " + expression);
+            set = new GenericSet<int>(expression, ',');
+            Console.WriteLine("The element string will be : " + set.ElementString);
+            Console.WriteLine("The cardinality will be : " + set.Cardinality);
+
+            expression = "{ 8, 7, 5, 2, 14, 1, { 5, 8, 9, 6, 33 } }";
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The expression is : " + expression);
+            set = new GenericSet<int>(expression, ',');
+            Console.WriteLine("The element string will be : " + set.ElementString);
+            Console.WriteLine("The cardinality will be : " + set.Cardinality);
+
+
+            expression = "{5,2,1,0}";
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The expression is : " + expression);
+            set = new GenericSet<int>(expression, ',');
+            Console.WriteLine("The element string will be : " + set.ElementString);
+            Console.WriteLine("The cardinality will be : " + set.Cardinality);
+
+            expression = "{ 8, 7, 5, 2, 14, 1, { 5, 8, 9, 6, 33 } }";
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The expression is : " + expression);
+            set = new GenericSet<int>(expression, ',');
+            Console.WriteLine("The element string will be : " + set.ElementString);
+            Console.WriteLine("The cardinality will be : " + set.Cardinality);
+
+            expression = "{5,5,3,{6,{5,8},5,6},7}";
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The expression is : " + expression);
+            set = new GenericSet<int>(expression, ',');
+            Console.WriteLine("The element string will be : " + set.ElementString);
+            Console.WriteLine("The cardinality will be : " + set.Cardinality);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Finnished!!!");
+            Console.Write("Press any key to see the generic set.......");
+            Console.ReadLine();
+        }//TestGenericSetWithNumbers
+        private static void TestGenericSetWithObjects()
+        {
+
+        }//TestGenericSetWithObjects
     }//class
 }//namespace
