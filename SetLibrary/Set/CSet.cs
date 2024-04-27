574d029ac018512988fa7a0572150c515e6bac7b
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-27 15:13:40 +0200

    Added a constructor to create an empty set.

--- SetLibrary/Set/CSet.cs
+++ SetLibrary/Set/CSet.cs
@@ -22,6 +22,10 @@ namespace SetLibrary
                 return this.tree[index];
             }//end getter
         }//End if
+        public CSet()
+        {
+            tree = new CSetTree<string>(new System.Collections.Generic.List<string>());
+        }//ctor default
         public CSet(string elementString)
         {
             BuildSet(elementString);
@@ -85,12 +89,14 @@ namespace SetLibrary
         }//IsSubSetOf
         public ISet<string> MergeWith(ISet<string> set)
         {
+            //Get the string representation of the sets
             string s1 = set.ToString();
             string s2 = this.ToString();
 
             //Now create the string representation of the new merged set
             string final = s1.Remove(s1.Length - 1) + "," + s2.Remove(0, 1);
 
+            //Return a new instance of the set
             return new CSet(final);
         }//MergeSets
         public ISet<string> Without(ISet<string> setB)
