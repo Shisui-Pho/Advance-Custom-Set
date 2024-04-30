22592a3e0b6b8490f2a936669d9b6466281b5899
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-30 10:47:05 +0200

    Implemented a test person class

--- Advanced Sets/Person.cs
+++ Advanced Sets/Person.cs
@@ -1,18 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
 namespace Advanced_Sets
 {
-    public class Person
+    public class Person : IEquatable<Person>, IComparable<Person>, IComparable
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public override string ToString()
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Person(string firstName, string lastName)
         {
-            return JsonSerializer.Serialize(this);
+            FirstName = firstName;
+            LastName = lastName;
+        }//ctor
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Person);
+        }//Equals
+
+        public bool Equals(Person other)
+        {
+            return other != null &&
+                   FirstName == other.FirstName &&
+                   LastName == other.LastName;
+        }//Equals
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + FirstName.GetHashCode();
+                hash = hash * 23 + LastName.GetHashCode();
+                return hash;
+            }
         }
+        public int CompareTo(Person other)
+        {
+            if (other == null)
+                return 1;
+
+            int lastNameComparison = LastName.CompareTo(other.LastName);
+            if (lastNameComparison != 0)
+                return lastNameComparison;
+
+            return FirstName.CompareTo(other.FirstName);
+        }//CompareTo
+
+        public int CompareTo(object obj)
+        {
+            return this.CompareTo((Person)obj);
+        }//CompareTo
     }//class
 }//namespace
