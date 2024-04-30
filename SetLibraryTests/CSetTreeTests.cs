6b08edab7f7f2cb0ad17b8f10d261d5fbad5567d
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-30 10:47:54 +0200

    Added more tests.

---
+++ SetLibraryTests/CSetTreeTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetLibraryTests
+{
+    public class CSetTreeTests
+    {
+        private ISet<string> tree;
+        public CSetTreeTests()
+        {
+            //tree = 
+        }//ctor
+
+
+
+    }//class
+}//namespace
