4e9e1f6844b9be060d1e45f56f23f227572e6dd4
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-04-30 11:06:03 +0200

    Added an interafce called ObjectConvert that contains a method that specifies the convertion of any objects

--- Advanced Sets/Person.cs
+++ Advanced Sets/Person.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SetLibrary.Generic;
+using SetLibrary.Objects_Sets;
+using System;
 namespace Advanced_Sets
 {
-    public class Person : IEquatable<Person>, IComparable<Person>, IComparable
+    public class Person : IObjectConverter<Person>, IComparable
     {
         public string FirstName { get; }
         public string LastName { get; }
@@ -12,43 +14,62 @@ namespace Advanced_Sets
             LastName = lastName;
         }//ctor
 
-        public override bool Equals(object obj)
+        public Person ToObject(string field, SetExtractionSettings<Person> settings)
         {
-            return Equals(obj as Person);
-        }//Equals
-
-        public bool Equals(Person other)
-        {
-            return other != null &&
-                   FirstName == other.FirstName &&
-                   LastName == other.LastName;
-        }//Equals
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hash = 17;
-                hash = hash * 23 + FirstName.GetHashCode();
-                hash = hash * 23 + LastName.GetHashCode();
-                return hash;
-            }
-        }
-        public int CompareTo(Person other)
-        {
-            if (other == null)
-                return 1;
-
-            int lastNameComparison = LastName.CompareTo(other.LastName);
-            if (lastNameComparison != 0)
-                return lastNameComparison;
-
-            return FirstName.CompareTo(other.FirstName);
-        }//CompareTo
+            string[] fields = field.Split(new string[] { settings.FieldTerminator }, StringSplitOptions.RemoveEmptyEntries);
+            if (fields.Length > 2)
+                throw new ArgumentException("Cannot convert to type of Person");
+            return new Person(fields[0], fields[1]);
+        }//ToObject
 
         public int CompareTo(object obj)
         {
-            return this.CompareTo((Person)obj);
+            return this.FirstName.CompareTo(((Person)obj).FirstName);
         }//CompareTo
     }//class
 }//namespace
+#region Alternative but not working for all cases
+/*
+ ALT
+
+
+        //public override bool Equals(object obj)
+        //{
+        //    return Equals(obj as Person);
+        //}//Equals
+
+        //public bool Equals(Person other)
+        //{
+        //    return other != null &&
+        //           FirstName == other.FirstName &&
+        //           LastName == other.LastName;
+        //}//Equals
+
+        //public override int GetHashCode()
+        //{
+        //    unchecked
+        //    {
+        //        int hash = 17;
+        //        hash = hash * 23 + FirstName.GetHashCode();
+        //        hash = hash * 23 + LastName.GetHashCode();
+        //        return hash;
+        //    }
+        //}
+        //public int CompareTo(Person other)
+        //{
+        //    if (other == null)
+        //        return 1;
+
+        //    int lastNameComparison = LastName.CompareTo(other.LastName);
+        //    if (lastNameComparison != 0)
+        //        return lastNameComparison;
+
+        //    return FirstName.CompareTo(other.FirstName);
+        //}//CompareTo
+
+        //public int CompareTo(object obj)
+        //{
+        //    return this.CompareTo((Person)obj);
+        //}//CompareTo
+*/
+#endregion
---
+++ SetLibrary/Objects Sets/IObjectConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SetLibrary.Generic;
+namespace SetLibrary.Objects_Sets
+{
+    /// <summary>
+    /// This interface defines specifies the convertion of a string to an objects of type T which can be used in the set
+    /// </summary>
+    /// <typeparam name="T">Object type that must also implement IComparable</typeparam>
+    public interface IObjectConverter<T>
+        where T : IComparable
+    {
+        T ToObject(string field, SetExtractionSettings<T> settings);
+    }//class
+}//namespace
