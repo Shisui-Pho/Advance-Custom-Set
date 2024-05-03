1ce9c63671e764cd45ec94dd7931e8ca8b249f66
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-05-03 08:52:15 +0200

    .

---
+++ SetLibrary/Exceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetLibrary
+{
+    public class FaildToExtractObjectException : Exception
+    {
+        string _msg;
+        public override string Message => _msg;
+        public FaildToExtractObjectException(string type, string msg) : base()
+        {
+            _msg = String.Format($"Failed to convert from string to {type}.");
+        }
+        public FaildToExtractObjectException(string message)
+            : base(message)
+        {
+        }//
+
+    }//class
+}//namespace
