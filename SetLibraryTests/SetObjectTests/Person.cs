1f3d09c5826d401fcb1d810845fec2db55ad1104
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-05-03 10:07:04 +0200

    Added tests for the set object.

---
+++ SetLibraryTests/SetObjectTests/Person.cs
@@ -0,0 +1,82 @@
+﻿using SetLibrary.Generic;
+using SetLibrary.Objects_Sets;
+using System;
+namespace SetLibraryTests.SetObjectTests
+{
+    public class Person : IObjectConverter<Person>, IComparable, IEquatable<Person>
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public Person() { }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }//ctor
+
+        public Person ToObject(string field, SetExtractionSettings<Person> settings)
+        {
+            string[] fields = field.Split(new string[] { settings.FieldTerminator }, StringSplitOptions.RemoveEmptyEntries);
+            if (fields.Length != 2)
+                throw new ArgumentException("Cannot convert to type of Person");
+            return new Person(fields[0], fields[1]);
+        }//ToObject
+        public int CompareTo(object obj)
+        {
+            return this.FirstName.CompareTo(((Person)obj).FirstName);
+        }//CompareTo
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }//ToString
+        public bool Equals(Person other)
+        {
+            return this.FirstName == other.FirstName && this.LastName == other.LastName;
+        }//Equals
+    }//class
+}//namespace
+#region Alternative but not working for all cases
+/*
+ ALT
+
+
+        //public override bool Equals(object obj)
+        //{
+        //    return Equals(obj as Person);
+        //}//Equals
+
+        //public bool Equals(Person other)
+        //{
+        //    return other != null &&
+        //           FirstName == other.FirstName &&
+        //           LastName == other.LastName;
+        //}//Equals
+
+        //public override int GetHashCode()
+        //{
+        //    unchecked
+        //    {
+        //        int hash = 17;
+        //        hash = hash * 23 + FirstName.GetHashCode();
+        //        hash = hash * 23 + LastName.GetHashCode();
+        //        return hash;
+        //    }
+        //}
+        //public int CompareTo(Person other)
+        //{
+        //    if (other == null)
+        //        return 1;
+
+        //    int lastNameComparison = LastName.CompareTo(other.LastName);
+        //    if (lastNameComparison != 0)
+        //        return lastNameComparison;
+
+        //    return FirstName.CompareTo(other.FirstName);
+        //}//CompareTo
+
+        //public int CompareTo(object obj)
+        //{
+        //    return this.CompareTo((Person)obj);
+        //}//CompareTo
+*/
+#endregion
---
+++ SetLibraryTests/SetObjectTests/SetObjectTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using Xunit;
+using SetLibrary;
+using SetLibrary.Generic;
+namespace SetLibraryTests.SetObjectTests
+{
+    using SetLibrary.Objects_Sets;
+    using Xunit;
+
+    public class SetObjectsTests
+    {
+        private SetExtractionSettings<Person> settings;
+
+        public SetObjectsTests()
+        {
+            settings = new SetExtractionSettings<Person>(",", " ", new Person());
+        }
+
+        [Fact]
+        public void Contains_ReturnsTrue_WhenPersonExistsInSet()
+        {
+            // Arrange
+            var set = new SetObjects<Person>("{John Doe, Alice Cooper, Bob Marley}", settings);
+            var personToFind = new Person("John", "Doe");
+
+            // Act
+            bool result = set.Contains(personToFind);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void MergeWith_CombinesTwoSetsCorrectly()
+        {
+            // Arrange
+            var setA = new SetObjects<Person>("{John Doe, Alice Cooper}", settings);
+            var setB = new SetObjects<Person>("{Bob Marley, Carol Johnson}", settings);
+            var expectedResult = new SetObjects<Person>("{John Doe, Alice Cooper, Bob Marley, Carol Johnson}", settings);
+
+            // Act
+            var mergedSet = setA.MergeWith(setB);
+
+            // Assert
+            Assert.Equal(expectedResult.ToString(), mergedSet.ToString());
+        }
+
+        [Fact]
+        public void FindFirstRootElement_ReturnsFirstPersonInSet()
+        {
+            // Arrange
+            var set = new SetObjects<Person>("{John Doe, Alice Cooper, Bob Marley}", settings);
+            var expectedPerson = new Person("Alice", "Cooper");
+
+            // Act
+            var firstPerson = set.FindFirstRootElement();
+
+            // Assert
+            Assert.Equal(expectedPerson, firstPerson);
+        }
+
+        [Fact]
+        public void GetSubSetElementByIndex_ReturnsSubsetAtIndex()
+        {
+            // Arrange
+            var set = new SetObjects<Person>("{{John Doe, Alice Cooper}, {Bob Marley, Carol Johnson}}", settings);
+            var expectedSubset = new SetObjects<Person>("{Bob Marley, Carol Johnson}", settings);
+
+            // Act
+            var subset = set.GetSubSetElementByIndex(1);
+
+            // Assert
+            Assert.Equal(expectedSubset.ToString(), subset.ToString());
+        }
+
+        [Fact]
+        public void GetRootElementByIndex_ReturnsElementAtIndex()
+        {
+            // Arrange
+            var set = new SetObjects<Person>("{{John Doe, Alice Cooper}, {Bob Marley, Carol Johnson}}", settings);
+            var expectedPerson = new Person("John", "Doe");
+
+            // Act
+            var person = set.GetRootElementByIndex(0);
+
+            // Assert
+            Assert.Equal(expectedPerson, person);
+        }
+    }
+
+}
