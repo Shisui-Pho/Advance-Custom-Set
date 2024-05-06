04a71999a47a4d5161f94a7cb447fe8274cbfd28
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-05-06 15:27:22 +0200

    Added an interface to define the SortedCollection lists which will be used to refere to the two types of collections in our application.

---
+++ SetLibrary/Model/ISortedSetCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetLibrary.Model
+{
+    internal interface ISortedSetCollection<T> : IEnumerable<T>
+        where T : IComparable
+    {
+        int Count { get; }
+        Element<T> this[int index] { get; }
+        void Add(T value);
+        void AddRange(IEnumerable<T> coll);
+        int IndexOf(T val);
+        bool Remove(T val);
+        void RemoveAt(int index);
+        bool Contains(T val);
+    }//class
+}//namespace
