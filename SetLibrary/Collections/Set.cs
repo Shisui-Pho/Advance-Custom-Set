15158328959307baa3eee8f1f63aba488097d781
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-05-20 16:43:41 +0200

    Added a Set structure which will be retuned in the set collection and will contain the information about the set including the name in the collection, element string and the cardinality

---
+++ SetLibrary/Collections/Set.cs
@@ -0,0 +1,15 @@
+﻿namespace SetLibrary
+{
+    public struct Set
+    {
+        public string Name { get; private set; }
+        public string ElementString { get; private set; }
+        public int Cardinality { get; private set; }
+        internal Set(string name, string set, int Cardinality)
+        {
+            this.Name = name;
+            this.ElementString = set;
+            this.Cardinality = Cardinality;
+        }//ctor
+    }//structure
+}//namespace
